94810cae1f3920080a06f21d5b542904908b8fe4
Rishab Ranjan <therishabranjan@gmail.com>
2023-05-15 14:32:42 +0530

    create survey changes

--- src/survey_components/CreateSurvey.jsx
+++ src/survey_components/CreateSurvey.jsx
@@ -1,26 +1,63 @@
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import { TextField } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import "./createsurvey.css";
 
 function CreateSurvey() {
+  const [isSurveyHeadingFocused, setIsSurveyHeadingFocused] = useState(false);
+  const [surveyName, setSurveyName] = useState("");
+  const [isSurveyTextFocused, setIsSurveyTextFocused] = useState(false);
+  const [surveyText, setSurveyText] = useState("");
+  const handleChange = (e) => {
+    if (e.target.name === "survey name") {
+      setSurveyName(e.target.value);
+    } else if (e.target.name === "survey description") {
+      setSurveyText(e.target.value);
+    }
+  };
   return (
     <div className="create-survey-main">
       <nav className="navbarHeading">
-        <p className="createSurveyHeading"> Create Survey</p>
+        <p className="createSurveyHeading">Create Survey</p>
         <p className="createSurveysubHeading">Home/MySurveys/CreateSurvey</p>
       </nav>
       <div className="create-survey-body">
-        <div>
+        <div className="mt-5">
           <h3 className="bodyHeading">Survey Subject & Description</h3>
         </div>
-        <TextField id="input1" placeholder="Survey Name" />
-        <div>
-          <TextField id="input2" placeholder="Describe your survey" />
+        <div className="textAreas d-flex flex-column mt-5">
+          <label htmlFor="descInput">
+            {(isSurveyHeadingFocused || surveyName) && "Survey Name"}
+          </label>
+          <input
+            onFocus={() => setIsSurveyHeadingFocused(true)}
+            onBlur={() => setIsSurveyHeadingFocused(false)}
+            onChange={handleChange}
+            placeholder="Survey Name"
+            value={surveyName}
+            type="text"
+            name="survey name"
+            id="descInput"
+          />
         </div>
-        <div className="footerline"></div>
-        <div>
-          <button className="btncs btn-light">
+        <div className="textAreas d-flex flex-column mt-5">
+          <label htmlFor="describeInput">
+            {(isSurveyTextFocused || surveyText) && "Describe your survey"}
+          </label>
+          <textarea
+            onFocus={() => setIsSurveyTextFocused(true)}
+            onBlur={() => setIsSurveyTextFocused(false)}
+            onChange={handleChange}
+            value={surveyText}
+            name="survey description"
+            id="describeInput"
+            cols="30"
+            rows="4"
+            placeholder="Describe your survey"
+          ></textarea>
+        </div>
+        <div className="footer">
+          <div className="footerline"></div>
+          <button className="btncs">
             NEXT
             <ArrowForwardIcon sx={{ marginLeft: 7 }} />
           </button>
